846bb4a45c36ea523b0d45c04c8ac96288e2bebc
Markel <markel2000es@yahoo.es>
2020-09-24 16:38:27 +0200

    segundo cambio

--- PruebaClaseHoy/PruebaClaseHoy/Program.cs
+++ PruebaClaseHoy/PruebaClaseHoy/Program.cs
@@ -6,7 +6,12 @@ namespace PruebaClaseHoy
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");kajnfjnajnsfj
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!"); kajnfjnajnsfjj
+                Console.WriteLine("Hello World!"); kajnfjnajnsfjj
+
+
         }
     }
 }
